cf9f3bec34d1e31189720edc6980bfcb681b7391
Cheo-Pog <698719@student.inholland.nl>
2024-04-07 19:51:01 +0200

    Update developmentdb.sql

--- sql/developmentdb.sql
+++ sql/developmentdb.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mysql
--- Generation Time: Jan 19, 2024 at 12:41 PM
--- Server version: 11.2.2-MariaDB-1:11.2.2+maria~ubu2204
--- PHP Version: 8.2.14
+-- Generation Time: Apr 07, 2024 at 05:50 PM
+-- Server version: 11.3.2-MariaDB-1:11.3.2+maria~ubu2204
+-- PHP Version: 8.2.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,23 +24,79 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Login`
+-- Table structure for table `category`
 --
 
-CREATE TABLE `Login` (
-  `id` int(255) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `rank` varchar(255) NOT NULL DEFAULT 'customer'
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `name` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `Login`
+-- Dumping data for table `category`
 --
 
-INSERT INTO `Login` (`id`, `username`, `password`, `rank`) VALUES
-(2, 'Jesse321', 'WEED', 'admin'),
-(12, 'waltuh', 'finger', 'customer');
+INSERT INTO `category` (`id`, `name`) VALUES
+(1, 'catss'),
+(2, 'shirts'),
+(3, 'skirts'),
+(4, 'hormones');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `totalPrice` decimal(65,2) NOT NULL,
+  `orderDate` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `userId`, `totalPrice`, `orderDate`) VALUES
+(9, 9, 5.97, '2024-04-06 19:22:53'),
+(10, 13, 219.80, '2024-04-06 20:10:18'),
+(11, 13, 146.79, '2024-04-07 17:48:51');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_items`
+--
+
+CREATE TABLE `order_items` (
+  `id` int(11) NOT NULL,
+  `orderId` int(11) NOT NULL,
+  `productId` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `price` decimal(65,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `order_items`
+--
+
+INSERT INTO `order_items` (`id`, `orderId`, `productId`, `quantity`, `price`) VALUES
+(4, 3, 23, 1, 6.99),
+(5, 3, 24, 2, 6.99),
+(6, 3, 25, 1, 0.01),
+(7, 4, 22, 1, 1000000.00),
+(8, 5, 22, 2, 1000000.00),
+(9, 5, 21, 1, 9.99),
+(10, 5, 25, 1, 0.01),
+(11, 6, 22, 1, 1000000.00),
+(12, 7, 21, 3, 9.99),
+(13, 7, 24, 2, 6.99),
+(14, 8, 24, 5, 6.99),
+(15, 9, 31, 3, 1.99),
+(16, 10, 32, 20, 10.99),
+(17, 11, 20, 21, 6.99);
 
 -- --------------------------------------------------------
 
@@ -54,7 +110,7 @@ CREATE TABLE `products` (
   `price` decimal(65,2) NOT NULL,
   `description` varchar(999) NOT NULL,
   `image` varchar(255) DEFAULT NULL,
-  `category` varchar(255) NOT NULL
+  `category` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -62,78 +118,149 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `price`, `description`, `image`, `category`) VALUES
-(19, 'cool cat', 999.00, 'this cat does not give a damn', 'uploads/doigiveadamn.jpg', 'cats'),
-(20, 'shirt', 6.99, 'this is a basic shirt', 'uploads/shirt.jpg', 'shirts'),
-(21, 'blue shirt', 9.99, 'a blue shirt', 'uploads/bshirt.jpg', 'shirts'),
-(22, 'MIKU', 1000000.00, 'omg, MIKU!', 'uploads/mikucat.jpg', 'cats'),
-(23, 'black skirt', 6.99, 'this is a black skirt', 'uploads/bskirt.jpg', 'skirts'),
-(24, 'brown skirt', 6.99, 'this is a brown skirt', 'uploads/8521c187-1040-4e1b-9daf-75c79e7d90fa.eae5e9e8d428f2a31a9a2a6d220eb445.jpg', 'skirts'),
-(25, 'estrogen', 0.01, 'hmmmm yummers', 'uploads/estrogen.jpg', 'hormones'),
-(26, 'testosterone', 0.02, 'hmmm yummy', 'uploads/testosterone.jpg', 'hormones');
+(20, 'shirt', 6.99, 'this is a basic shirt', '/uploads/66118f84e620c.jpg', 2),
+(22, 'MIKU', 1000000.00, 'omg, MIKU!', '/uploads/66118fad0fcea.jpg', 1),
+(23, 'black skirt', 6.99, 'this is a black skirt', '/uploads/66118f3a1124b.jpg', 3),
+(25, 'estrogen', 0.01, 'hmmmm yummers', '/uploads/66118f71cb9a1.jpg', 4),
+(26, 'testosterone', 0.02, 'hmmm yummy', '/uploads/66118f65752bc.jpg', 4),
+(32, 'blue shirt', 10.99, 'this is a blue shirt', '/uploads/66118fbf5cfb3.jpg', 2),
+(33, 'cool skirt', 1.99, 'hella ', '/uploads/66118efd4ed11.png', 3);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `shoppingcart`
+-- Table structure for table `shoppingcarts`
 --
 
-CREATE TABLE `shoppingcart` (
-  `orderId` int(255) NOT NULL,
-  `userId` int(255) NOT NULL,
-  `productId` int(255) NOT NULL,
-  `quantity` int(255) NOT NULL
+CREATE TABLE `shoppingcarts` (
+  `id` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `productId` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `shoppingcart`
+-- Dumping data for table `shoppingcarts`
 --
 
-INSERT INTO `shoppingcart` (`orderId`, `userId`, `productId`, `quantity`) VALUES
-(5, 8, 2, 1);
+INSERT INTO `shoppingcarts` (`id`, `userId`, `productId`, `quantity`) VALUES
+(30, 4, 21, 9),
+(31, 4, 22, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `firstName` varchar(20) NOT NULL,
+  `lastName` varchar(20) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `rank` tinyint(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `firstName`, `lastName`, `password`, `rank`) VALUES
+(9, 'soph@gmail.com', 'Soph', 'whocares', '$2y$10$pmPZENcXtts8KE/C6iiJE.rb0TCREmlfIuBDJHJb.WALt5rPN2k92', 2),
+(13, 'mr@heisenberg.cool', 'Waltuh', 'White', '$2y$10$jrUjRGpHnHy/XXUTFz3l3ueBlhRA5u.Gap8VwLxpED4yNGu0Gp0MW', 2),
+(21, 'milfHunter@hotmail.com', 'Jesse', 'Pinkman', '$2y$10$qc/8PbCYwHq.m0XMUdp1guNd3TlJHfcHb/6ShAKMpbC8rK8BeWm8m', 1);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `Login`
+-- Indexes for table `category`
 --
-ALTER TABLE `Login`
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `order_items`
+--
+ALTER TABLE `order_items`
   ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `products`
 --
 ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `category` (`category`);
+
+--
+-- Indexes for table `shoppingcarts`
+--
+ALTER TABLE `shoppingcarts`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `shoppingcart`
+-- Indexes for table `users`
 --
-ALTER TABLE `shoppingcart`
-  ADD PRIMARY KEY (`orderId`);
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `Login`
+-- AUTO_INCREMENT for table `category`
 --
-ALTER TABLE `Login`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+ALTER TABLE `category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `order_items`
+--
+ALTER TABLE `order_items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
--- AUTO_INCREMENT for table `shoppingcart`
+-- AUTO_INCREMENT for table `shoppingcarts`
 --
-ALTER TABLE `shoppingcart`
-  MODIFY `orderId` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+ALTER TABLE `shoppingcarts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`category`) REFERENCES `category` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
